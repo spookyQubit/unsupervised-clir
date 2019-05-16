f1546bd57a4690fae31faced8a7eb9efda9fdbb7
spookyQubit <iitkgp.shantanu@gmail.com>
2019-05-15 21:36:28 -0400

    docker files

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM conda/miniconda3
+MAINTAINER Shantanu Agarwal shan@gmail.com
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+
--- download_unzip.sh
+++ download_unzip.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# DOwnloads file zipped file from a websource=url and unzips it is destination=directory 
+
+# usage 
+# ./download_unzip.sh  -s "madata.bib.uni-mannheim.de/273/1/UnsupCLIREmbeddings.tar.gz"
+# ./download_unzip.sh  -s "www.cs.jhu.edu/~kevinduh/a/wikiclir2018/sasaki18.tgz" -d Data
+ 
 DESTINATION=.
 
 POSITIONAL=()
