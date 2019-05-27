5954fa7f12cb77417d7253c2afe0ce671e6b8be5
spookyQubit <iitkgp.shantanu@gmail.com>
2019-05-27 03:46:08 -0400

    main updated

--- src/main.py
+++ src/main.py
@@ -1,8 +1,51 @@
 import sys
+from os import path
+
+g_wikiclir2018_dir = "../Data/wikiclir2018"
+g_query_wikiclir2018_language = "en"
+g_doc_wikiclir2018_languages = ["fr", "ja"]
+
+# required for stopwords
+g_wikiclir2018_language_to_nltk_language = {"en": "english", 
+                                            "fr": "french", 
+                                            "ja": "japanese"}  
+g_data_mode = 'dev'
+g_data_mode_to_f_name = {'dev': 'dev', 'test': 'test1', 'train': 'train'}
 
 
-def process_and_store_data():
-    pass
+def get_input_and_output_data_file_names(wiki_dir, 
+                                         query_wiki_lang, 
+                                         doc_wiki_lang, 
+                                         f_name):
+    '''
+    param wiki_dir: Relative path to where wikiclir2018 data is stored
+    param query_wiki_lang: Query language. Assumed to be compatible with 
+                           lang in stopwords.words(lang)
+    param doc_wiki_lang: Doc language. Assumed to be compatible with 
+                         lang in stopwords.words(lang)
+    param f_name: train or dev or test1 depending upon the data_mode
+    returns: in_file_path, that is the file from which to read data
+             out_file_path, that is the file where to store processed data
+    '''
+
+    data_dir = '_'.join(['data', query_wiki_lang, doc_wiki_lang])
+
+    in_f_name = f_name + '.txt'
+    out_f_name = f_name + '_processed' + '.txt'
+    
+    in_file_path = path.join(wiki_dir, data_dir, in_f_name)
+    out_file_path = path.join(wiki_dir, data_dir, out_f_name)
+
+    return in_file_path, out_file_path
+
+
+def process_and_store_data(input_file, output_file, query_lang, doc_lang):
+
+    if not path.exists(input_file):
+        raise Exception("{} does not exist".format(input_file))
+
+
+
 
 def main():
     # load word embeddings
@@ -11,8 +54,15 @@ def main():
     # process query and texts
     # run experiment 
 
+    for doc_wiki_lang in g_doc_wikiclir2018_languages:
+        in_file_path, out_file_path = get_input_and_output_data_file_names(g_wikiclir2018_dir, 
+                                                                           g_query_wikiclir2018_language, 
+                                                                           doc_wiki_lang, 
+                                                                           g_data_mode_to_f_name[g_data_mode])
 
-    process_and_store_data()
+        process_and_store_data(in_file_path, out_file_path, 
+                               query_lang=g_wikiclir2018_language_to_nltk_language[g_query_wikiclir2018_language], 
+                               doc_lang=g_wikiclir2018_language_to_nltk_language[doc_wiki_lang])
 
 
 if __name__=="__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,31 @@
 import unittest
+import sys
+
+sys.path.insert(0, '../')
+from src import main
 
 
-class TestCalc(unittest.TestCase):
+class TestMain(unittest.TestCase):
 
-    def add_test(self):
-        result = main.add(10, 5)
-        self.assertEqual(result, 5)
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_get_input_and_output_data_file_names(self):
+        wiki_dir='../DataDir' 
+        query_wiki_lang='qLang' 
+        doc_wiki_lang='dLang'
+        f_name='dev'
+
+        in_path, out_path = main.get_input_and_output_data_file_names(wiki_dir, 
+                                                                      query_wiki_lang, 
+                                                                      doc_wiki_lang, 
+                                                                      f_name)
+        self.assertEqual(in_path, '../DataDir/data_qLang_dLang/dev.txt')
+        self.assertEqual(out_path, '../DataDir/data_qLang_dLang/dev_processed.txt')
+
 
 if __name__ == "__main__":
     unittest.main()
